5cab9748d947a3b17a133d714d2d864af96b1f1a
sharmila <48220@edu.raseko.fi>
2023-01-27 14:50:46 +0200

    Simple calculator in Python
    Learning basic in python

---
+++ calculator_python.py
@@ -0,0 +1,24 @@
+first_number = input("Enter first number: ")
+operator = input("Enter any Operator (+,-,*,/,%): ")
+second_number = input("Enter second number: ")
+
+first_number = int(first_number)
+second_number = int(second_number)
+
+if operator == "+":
+    print(first_number + second_number)
+
+elif operator == "-":
+    print(first_number - second_number)
+
+elif operator == "*":
+    print(first_number * second_number)
+
+elif operator == "/":
+    print(first_number / second_number)
+
+elif operator == "%":
+    print(first_number % second_number)
+
+else:
+    print("invalid numbers")
