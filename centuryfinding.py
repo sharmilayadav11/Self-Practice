1a8ca3922761f2253237e40c00f1e2f099ce3269
sharmila <48220@edu.raseko.fi>
2023-03-14 00:00:10 +0200

    if and else statement
    if and else statement

---
+++ centuryfinding.py
@@ -0,0 +1,7 @@
+# Make an if-else statement where if year is between 2000 and 2099, then print out:
+
+year = int(input("Enter Year: "))
+if 2000 <= year < 2100:
+    print("Welcome to the 21st century!")
+else:
+    print("You are before or after the 21st century")
---
+++ pythonrefresher.py
@@ -0,0 +1,11 @@
+name = 'Sharmila'
+age = 17
+print(f'Hello, my name is {name} and I am {age} years old')
+""""
+if else statement in python
+"""
+if age != 14:
+    print("Your are older than 18")
+    print("Your are so beautiful")
+else:
+    print("You are younger than 18")
